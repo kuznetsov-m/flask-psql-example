cac3687e921ee974331f2cef502c091165b76d29
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2021-02-15 00:38:08 +0300

    Hello flask

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
-
-
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-from models import Result
 
 
 @app.route('/')
@@ -19,7 +10,6 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
-
 
 if __name__ == '__main__':
     app.run()
